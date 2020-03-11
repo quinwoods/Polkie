2ee2ff16a8c2c25e745b36ed9520c23b09a92ac9
Quin'darius <quinwoodz@gmail.com>
2020-03-11 06:37:58 -0400

    Initial Commit

---
+++ Polkie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Polkie
+//
+//  Created by Quin’darius Lyles-Woods on 3/11/20.
+//  Copyright © 2020 Polkie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
